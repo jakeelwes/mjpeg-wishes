1c09bf15aef8c022ec3e9a13a26531275d352ab7
Emmanuel Geoffray <manuel@soixantecircuits.fr>
2013-12-26 19:23:49 +0100

    added date and time

--- generate_gif.sh
+++ generate_gif.sh
@@ -1,10 +1,10 @@
 GIFPATH=stream/togif
 mkdir $GIFPATH
 rm $GIFPATH/snap*.jpg
-for f in stream*.jpg; do cp "$f" $GIFPATH/"$f"; done
-for f in stream*.jpg; do cp "$f" $GIFPATH/2"$f"; done
-for f in stream*.jpg; do cp "$f" $GIFPATH/3"$f"; done
-find stream/ -name \*.jpg -printf "%C+ %h/%f\n" | sort -r | head -n70 | awk '{print "\""$2"\""}' | xargs -I {} cp {} $GIFPATH/
+for f in stream_*.jpg; do cp --preserve=timestamps "$f" $GIFPATH/"$f"; done
+for f in stream_*.jpg; do cp --preserve=timestamps "$f" $GIFPATH/2"$f"; done
+for f in stream_*.jpg; do cp --preserve=timestamps "$f" $GIFPATH/3"$f"; done
+find stream/ -name \*.jpg -printf "%C+ %h/%f\n" | sort -r | head -n70 | awk '{print "\""$2"\""}' | xargs -I {} cp --preserve=timestamps {} $GIFPATH/
 mogrify -shave 0x16 -resize 500x355 $GIFPATH/snap*.jpg
 python watermark.py $GIFPATH
 convert -delay 15 -loop 0 -colors 16 $GIFPATH/*.jpg $GIFPATH/sunrise_im.gif
--- watermark.py
+++ watermark.py
@@ -1,11 +1,13 @@
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 import os
 import sys
+import os.path, time
+import datetime
  
-def process(filename):
+def process(filename, path, original_path):
 # Open the original image
-  main = Image.open(filename)
-  text_as_img = Image.open("no_time.png")
+  main = Image.open(path + '/' + filename)
+  text_as_img = Image.open("mask.png")
   
 # Create a new image for the watermark with an alpha layer (RGBA)
 # the same size as the original image
@@ -13,7 +15,13 @@ def process(filename):
 # Get an ImageDraw object so we can draw on the image
   waterdraw = ImageDraw.ImageDraw(watermark, "RGBA")
 # Place the text at (10, 10) in the upper left corner. Text will be white.
-  waterdraw.text((10, 10), "The Image Project")
+  font = ImageFont.truetype("Brandon_blk.otf", 22)
+  modifiedTime = os.path.getmtime(original_path + '/' + filename)
+  str_time = datetime.datetime.fromtimestamp(modifiedTime).strftime('%H:%M:%S')
+  str_day = datetime.datetime.fromtimestamp(modifiedTime).strftime('%B %d, %Y')
+  waterdraw.text((300, 284), str_time, font= font)
+  waterdraw.text((46, 284), str_day.upper(), font= font)
+  #waterdraw.text((300, 284), "11:03:08", font= font)
    
 # Get the watermark image as grayscale and fade the image
 # See <http://www.pythonware.com/library/pil/handbook/image.htm#Image.point>
@@ -22,21 +30,22 @@ def process(filename):
 # how faded the image will be. That number is in the range [0, 256],
 # where 0 is black and 256 is white. A good value for fading our white
 # text is in the range [100, 200].
-  watermask = watermark.convert("L").point(lambda x: min(x, 100))
+  #watermask = watermark.convert("L").point(lambda x: min(x, 100))
 # Apply this mask to the watermark image, using the alpha filter to
 # make it transparent
-  watermark.putalpha(watermask)
+  #watermark.putalpha(watermask)
     
 # Paste the watermark (with alpha layer) onto the original image and save it
   #main.paste(watermark, None, watermark)
-  #main.paste(watermark, None, watermask)
+  main.paste(watermark, None, watermark)
   main.paste(text_as_img, (0,0), text_as_img)
-  main.save(filename, "JPEG")
+  main.save(path + '/' + filename, "JPEG")
    
 if __name__ == '__main__':
   path = 'stream/togif'
+  original_path = 'stream'
   if sys.argv[1]:
     path = sys.argv[1]+'/'
   file_names = sorted((fn for fn in os.listdir(path) if fn.startswith('snap')))
   for im in file_names:
-    process(path + im)
+    process(im, path, original_path)
