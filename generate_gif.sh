2c313d772bfc04e9a141afde2eadd4aa180a378b
Gabriel Delattre <gabriel@soixantecircuits.fr>
2013-12-26 20:09:11 +0100

    it is better to mv file rather than cp for a non-glitch serving

--- generate_gif.sh
+++ generate_gif.sh
@@ -8,5 +8,5 @@ find stream/ -name \*.jpg -printf "%C+ %h/%f\n" | sort -r | head -n70 | awk '{pr
 mogrify -shave 0x16 -resize 500x355 $GIFPATH/snap*.jpg
 python watermark.py $GIFPATH
 convert -delay 15 -loop 0 -colors 16 $GIFPATH/*.jpg $GIFPATH/sunrise_im.gif
-cp $GIFPATH/sunrise_im.gif /var/www/petit-prince/sunrise_im.gif
+mv $GIFPATH/sunrise_im.gif /var/www/petit-prince/sunrise_im.gif
 
