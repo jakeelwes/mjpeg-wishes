e887e3c40fa56763fb02a92d3a9df76fcfea0efa
Emmanuel Geoffray <manuel@soixantecircuits.fr>
2014-01-08 17:30:04 +0100

    Fix for older versions of imagemagick

--- generate_clean_gif.sh
+++ generate_clean_gif.sh
@@ -6,8 +6,9 @@ if ls $1/snap*.jpg > /dev/null 2>&1; then
     mkdir -p $WWWPATH
     mkdir -p archives
     TS=$( date +%Y%m%d%H%M%S )
-    find $1/ -name \*.jpg -printf "%C+ %h/%f\n" | sort -r | head -n70 | awk '{print "\""$2"\""}' | sed -n '1!G;h;$p' > $FILELIST 
-    convert -delay 15 -loop 0 -colors 64 @$FILELIST $GIFPATH/$1.gif
+    find $1/ -name \*.jpg -printf "%C+ %h/%f\n" | sort -r | head -n70 | awk '{print ""$2""}' | sed -n '1!G;h;$p' > $FILELIST 
+    LIST=$(cat $FILELIST)
+    convert -delay 15 -loop 0 -colors 64 $LIST $GIFPATH/$1.gif
     mv $GIFPATH/$1.gif $WWWPATH/$1-$TS.gif
     echo "{\"last\":\"$1-$TS.gif\"}" > $WWWPATH/last.json
   else
