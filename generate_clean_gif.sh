1169865cdc47c22d4644f8a2eeaca09d9854e552
Gabriel Delattre <gabriel@soixantecircuits.fr>
2014-01-08 17:53:00 +0100

    fix generate gif with loading

--- generate_clean_gif.sh
+++ generate_clean_gif.sh
@@ -1,5 +1,5 @@
 GIFPATH=$1/cleangif
-WWWPATH=/var/www/soixantesunrise/$1
+WWWPATH=/var/www/soixantesunrises/$1
 FILELIST=$GIFPATH/images_list.txt
 if ls $1/snap*.jpg > /dev/null 2>&1; then
     mkdir -p $GIFPATH
@@ -9,7 +9,8 @@ if ls $1/snap*.jpg > /dev/null 2>&1; then
     find $1/ -name \*.jpg -printf "%C+ %h/%f\n" | sort -r | head -n70 | awk '{print ""$2""}' | sed -n '1!G;h;$p' > $FILELIST 
     LIST=$(cat $FILELIST)
     convert -delay 15 -loop 0 -colors 64 $LIST $GIFPATH/$1.gif
-    mv $GIFPATH/$1.gif $WWWPATH/$1-$TS.gif
+    mv $GIFPATH/$1.gif $WWWPATH/last.gif
+    cp $WWWPATH/last.gif $WWWPATH/$1-$TS.gif
     echo "{\"last\":\"$1-$TS.gif\"}" > $WWWPATH/last.json
   else
     echo "No files in $1"
