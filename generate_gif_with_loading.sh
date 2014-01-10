e3e1ed76b0faf7a4ec7ca81df63e70b79a1e6180
Gabriel Delattre <gabriel@soixantecircuits.fr>
2014-01-10 15:17:30 +0100

    cams

--- generate_gif_with_loading.sh
+++ generate_gif_with_loading.sh
@@ -1,11 +1,11 @@
 GIFPATH=$1/cleangif
 WWWPATH=/var/www/soixantesunrises/$1
-GIFMAILPATH=/var/www/soixantesunrises/
+GIFMAILPATH=/var/www/soixantesunrises
 if ls $WWWPATH/*.gif > /dev/null 2>&1; then
     #GIF=$(find $WWWPATH/ -name \*.gif -printf "%C+ %h/%f\n" | sort -r | head -n1 | awk '{print ""$2""}') 
     GIF=last.gif
-    convert -delay 15 -loop 0 -colors 64 loading.gif $GIF $GIFPATH/$1_im.gif
-    mv $GIFPATH/$1_im.gif /var/www/soixantesunrises/sunrise_im.gif
+    convert -delay 15 -loop 0 -colors 64 loading.gif $WWWPATH/$GIF $GIFPATH/$1_im.gif
+    mv $GIFPATH/$1_im.gif $GIFMAILPATH/sunrise_im.gif
   else
     echo "No gif for $1, can't create mailgif"
 fi
